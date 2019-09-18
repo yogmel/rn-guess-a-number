8fc30d5fd0447427b853a8ecf558f8b8d1a90f56
yogmel <pong7219@gmail.com>
2019-09-18 16:26:19 -0300

    Componetização do Card.js e criação de constantes de cor

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
+import Card from '../components/Card';
+import Colors from './../constants/colors';
+
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Reset" onPress={() => {}} />
-          <Button title="Confirm" onPress={() => {}} />
+          <View  style={styles.button}>
+            <Button color={Colors.primary} title="Reset" onPress={() => {}} />
+          </View>
+          <View  style={styles.button}> 
+            <Button color={Colors.accent} title="Confirm" onPress={() => {}} />
+          </View>
         </View>
-      </View>
+      </Card>
     </View>
   )
 };
@@ -26,6 +33,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     marginVertical: 10
+  },
+  button: {
+    width: '100%',
+    maxWidth: 80
   },
   inputContainer: {
     width: 300,
