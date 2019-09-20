8e6fca5f6724d3a2f28344501c61e02172683164
yogmel <pong7219@gmail.com>
2019-09-20 15:52:01 -0300

    Inserção de imagem no GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
 import BodyText from './../components/BodyText';
+import TitleText from './../components/TitleText';
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <BodyText>
+      <TitleText>
         The Game is Over!
-      </BodyText>
-      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      </TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          // source={require('./../assets/success.png')}
+          source={{uri: 'https://www.petmd.com/sites/default/files/Senior-Cat-Care-2070625.jpg'}}
+          style={styles.image}
+          resizeMode="cover"  
+        />
+      </View>
+      <View style={styles.textContainer}>
+        <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+        <BodyText>Number was: {props.userNumber}</BodyText>
+      </View>
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   )
@@ -21,6 +32,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 200,
+    borderWidth: 2,
+    borderColor: 'black',
+    overflow: 'hidden',
+    marginVertical: 20
+  },
+  image: {
+    width: '100%',
+    height: '100%'
+  },
+  textContainer: {
+    marginBottom: 20
   }
 });
 
